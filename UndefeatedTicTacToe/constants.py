1a49668ae403642246d571d8c35e6c8313de4d98
linnix41 <69366534+linnix41@users.noreply.github.com>
2022-07-28 18:20:28 +0300

    Add files via upload

---
+++ UndefeatedTicTacToe/constants.py
@@ -0,0 +1,27 @@
+# ---------
+# CONSTANTS
+# ---------
+
+# --- PIXELS ---
+
+WIDTH = 720
+HEIGHT = 720
+
+ROWS = 3
+COLS = 3
+SQSIZE = WIDTH // COLS
+
+LINE_WIDTH = 15
+CIRC_WIDTH = 15
+CROSS_WIDTH = 20
+
+RADIUS = SQSIZE // 4
+
+OFFSET = 50
+
+# --- COLORS ---
+
+BG_COLOR = (255, 180, 180)
+LINE_COLOR = (93, 95, 135)
+CIRC_COLOR = (239, 231, 200)
+CROSS_COLOR = (66, 66, 66)
